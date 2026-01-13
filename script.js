65f22d3b39330d9988350f9b0545d0f27db7ef60
JesseLeeOnFB <Skeeterjeeter8@gmail.com>
2026-01-12 20:36:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ import {
   onAuthStateChanged,
   signOut
 } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHMbxr7rJS88ZefVJzt8p_9CCTstLmLU8",
+  authDomain: "yourspace-2026.firebaseapp.com",
+  projectId: "yourspace-2026",
+  storageBucket: "yourspace-2026.firebasestorage.app",
+  messagingSenderId: "72667267302",
+  appId: "1:72667267302:web:2bed5f543e05d49ca8fb27",
+  measurementId: "G-FZ4GFXWGSS"
+};
 
 import {
   collection,
@@ -142,3 +152,4 @@ onSnapshot(postsQuery, snapshot => {
     feed.appendChild(postEl);
   });
 });
+

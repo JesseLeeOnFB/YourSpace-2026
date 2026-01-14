92219458559304c7bf0be90a0e391b99d8d43f95
JesseLeeOnFB <Skeeterjeeter8@gmail.com>
2026-01-14 03:58:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAHMbxr7rJS88ZefVJzt8p_9CCTstLmLU8",
   authDomain: "yourspace-2026.firebaseapp.com",
   projectId: "yourspace-2026",
-  storageBucket: "yourspace-2026.appspot.com"
+  storageBucket: "yourspace-2026.appspot.com", // <-- must be .appspot.com
   messagingSenderId: "72667267302",
   appId: "1:72667267302:web:2bed5f543e05d49ca8fb27",
   measurementId: "G-FZ4GFXWGSS"
